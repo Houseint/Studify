36fb5e18817bdbbbaa895741300f920dc318f733
Gabriel Rodrigues <gabriel.silva3804@etec.sp.gov.br>
2025-09-09 08:40:50 -0300

    Base da tela feita

--- Studify/src/HomeScreen.js
+++ Studify/src/HomeScreen.js
@@ -5,20 +5,37 @@ import { View, Image, StyleSheet, SectionList, Text, TouchableOpacity} from 'rea
 
 const HomeScreen = ({navigator}) => {
     return (
+        // Main Container
         <View style={styles.container}>
-            <View style={styles.header}>
-                
-                <TouchableOpacity onPress={() => navigator.openDrawer()}>
-                    <Image source={require('../img/menu.png')} style={styles.menu} />
-                </TouchableOpacity>
-
-
-                <TouchableOpacity onPress={() => navigator.navigate()}>
-                    <Image source={require('../img/livro.png')} style={styles.logo} />
-                </TouchableOpacity>
-                
-                
+            <View>
+                <Text style={styles.title}>Últimos conteúdos acessados</Text>
+                <Image source={require('../img/relogio.png')} style={styles.frames} />
             </View>
+
+            <View>
+                <Text style={styles.title}>Conteúdos para serem revisados</Text>
+                <Image source={require('../img/livro.png')} style={styles.frames} /> /*Colocar a lupa depois*/
+            </View>
+
+            <View>
+                <Text style={styles.title}>Recomendações</Text>
+                <Image source={require('../img/menu.png')} style={styles.frames} /> /*Colocar a bandeirinha de salvar*/
+            </View>
+
+        // Header
+        <View style={styles.header}>
+            <TouchableOpacity onPress={() => navigator.openDrawer()}>
+                <Image source={require('../img/menu.png')} style={styles.menu} />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => navigator.navigate()}>
+                <Image source={require('../img/livro.png')} style={styles.logo} />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => navigator.navigate()}>
+                <Image source={require('../img/relogio.png')} style={styles.logo} />
+            </TouchableOpacity>
+        </View>
         </View>
     );
 }
