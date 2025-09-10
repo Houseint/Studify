044257c9154e30b1d7c705ad1d0e667b861a87d0
GABRIEL RODRIGUES DE MELO SILVA <gabriel.silva3804@etec.sp.gov.br>
2025-09-10 10:10:13 -0300

    Alguns ajustes

--- Studify/src/HomeScreen.js
+++ Studify/src/HomeScreen.js
@@ -6,54 +6,80 @@ import { View, Image, StyleSheet, SectionList, Text, TouchableOpacity} from 'rea
 const HomeScreen = ({navigator}) => {
     return (
         // Main Container
-        <View style={styles.container}>
-            <View>
-                <Text style={styles.title}>Últimos conteúdos acessados</Text>
-                <Image source={require('../img/relogio.png')} style={styles.frames} />
+        <View style={styles.main}>
+            <View style={styles.buttons}>
+                <TouchableOpacity onPress={() => navigator.navigate('')}>
+                    <Image source={require('../img/menu.png')} style={styles.logo} />
+                </TouchableOpacity>
             </View>
 
-            <View>
-                <Text style={styles.title}>Conteúdos para serem revisados</Text>
-                <Image source={require('../img/livro.png')} style={styles.frames} /> /*Colocar a lupa depois*/
+            <View style={styles.container}>
+                <View>
+                    <Text style={styles.title}>Últimos conteúdos acessados</Text>
+                    <Image source={require('../img/Relogio-iconP.png')} style={styles.frames} />
+                </View>
+
+                <View>
+                    <Text style={styles.title}>Conteúdos para serem revisados</Text>
+                    <Image source={require('../img/Pesquisar-iconP.png')} style={styles.frames} />
+                </View>
+
+                <View>
+                    <Text style={styles.title}>Recomendações</Text>
+                    <Image source={require('../img/FitaBookMark-icon.png')} style={styles.frames} />
+                </View>
             </View>
 
-            <View>
-                <Text style={styles.title}>Recomendações</Text>
-                <Image source={require('../img/menu.png')} style={styles.frames} /> /*Colocar a bandeirinha de salvar*/
+            {/* Header */}
+            <View style={styles.header}>
+                <TouchableOpacity onPress={() => navigator.navigate('')}>
+                    <Image source={require('../img/Pesquisar-iconG.png')} style={styles.logo} />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => navigator.navigate('')}>
+                    <Image source={require('../img/livro.png')} style={styles.logo} />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => navigator.navigate('')}>
+                    <Image source={require('../img/Relogio-iconG.png')} style={styles.logo} />
+                </TouchableOpacity>
             </View>
-
-        // Header
-        <View style={styles.header}>
-            <TouchableOpacity onPress={() => navigator.openDrawer()}>
-                <Image source={require('../img/menu.png')} style={styles.menu} />
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={() => navigator.navigate()}>
-                <Image source={require('../img/livro.png')} style={styles.logo} />
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={() => navigator.navigate()}>
-                <Image source={require('../img/relogio.png')} style={styles.logo} />
-            </TouchableOpacity>
-        </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    main: {
         flex: 1,
         backgroundColor: '#22272C',
+        paddingLeft: 12,
+        paddingRight: 12,
     },
+
+    container: {
+        flex: 1,
+        marginTop: '10%',
+        marginLeft: '5%',
+    },
+
+    title: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        fontFamily: 'Roboto',
+        marginTop: 20,
+        marginLeft: 10,
+    },
+
     header: {
-        backgroundColor: '2E343B',
+        backgroundColor: '#4C545A',
+        borderRadius: '5%',
         flexDirection: 'row',
         justifyContent: 'space-between',
-        position: 'absolute',
         width: '100%',
         height: '7.5%',
-        bottom: 0,
-        gap: 20,
+        bottom: '2.5%',
+        alignItems: 'center',
+        padding: '5%',
     },
 });
 
